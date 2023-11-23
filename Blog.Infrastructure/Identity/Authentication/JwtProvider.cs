85f50cead259898d55ea7dfa51babc69211a17fd
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-23 16:29:44 +0700

    Add JwtProvider

---
+++ Blog.Infrastructure/Identity/Authentication/JwtProvider.cs
@@ -0,0 +1,40 @@
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Blog.Infrastructure.Identity.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Infrastructure.Identity.Authentication;
+
+internal sealed class JwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOptions> jwtOptions) =>
+        _jwtOptions = jwtOptions.Value;
+
+    public string Generate(ApplicationUser user)
+    {
+        var claims = new Claim[]
+        {
+            new (JwtRegisteredClaimNames.Sub, user.Id),
+            new (JwtRegisteredClaimNames.Email, user.Email ?? ""),
+            new (JwtRegisteredClaimNames.Jti, new Guid().ToString()),
+            new (JwtRegisteredClaimNames.Gender, user.Gender.ToString()),
+        };
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        
+        var token = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtOptions.DurationInMinutes), 
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
