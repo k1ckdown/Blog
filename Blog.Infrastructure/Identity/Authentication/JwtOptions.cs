bbb969aa0ab97212a4abc4037e5bd5b43601a4e7
Ivan Semenov <DevSemTsk@gmail.com>
2023-11-23 16:05:32 +0700

    Add JWT options - added options to appsettings - added options model

---
+++ Blog.Infrastructure/Identity/Authentication/JwtOptions.cs
@@ -0,0 +1,9 @@
+namespace Blog.Infrastructure.Identity.Authentication;
+
+public class JwtOptions
+{
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public double DurationInMinutes { get; set; }
+}
