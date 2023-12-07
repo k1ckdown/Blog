2aaa80d4e4c5c826040f555b54db207ff317da11
Ivan Semenov <DevSemTsk@gmail.com>
2023-12-07 18:10:30 +0700

    Add access check when adding and removing likes

--- Application/Features/Post/Commands/DislikePost/DislikePostCommandHandler.cs
+++ Application/Features/Post/Commands/DislikePost/DislikePostCommandHandler.cs
@@ -1,5 +1,6 @@
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Repositories;
+using Application.Services.Community;
 using MediatR;
 
 namespace Application.Features.Post.Commands.DislikePost;
@@ -7,19 +8,22 @@ namespace Application.Features.Post.Commands.DislikePost;
 public sealed class DislikePostCommandHandler : IRequestHandler<DislikePostCommand>
 {
     private readonly IPostRepository _postRepository;
+    private readonly ICommunityService _communityService;
 
-    public DislikePostCommandHandler(IPostRepository postRepository)
+    public DislikePostCommandHandler(IPostRepository postRepository, ICommunityService communityService)
     {
         _postRepository = postRepository;
+        _communityService = communityService;
     }
 
     public async Task Handle(DislikePostCommand request, CancellationToken cancellationToken)
     {
-        if (await _postRepository.GetByIdAsync(request.PostId) == null)
-            throw new NotFoundException(nameof(Domain.Entities.Post), request.PostId);
+        var post = await _postRepository.GetByIdAsync(request.PostId);
+        if (post == null) throw new NotFoundException(nameof(Domain.Entities.Post), request.PostId);
+        
+        await _communityService.CheckAccessToPost(request.UserId, post);
         
         var like = await _postRepository.GetLikeAsync(request.UserId, request.PostId);
-
         if (like == null)
             throw new NotFoundException(
                 $"The user ({request.UserId}) doesn't have like on the post ({request.PostId})");
--- Application/Features/Post/Commands/LikePost/LikePostCommandHandler.cs
+++ Application/Features/Post/Commands/LikePost/LikePostCommandHandler.cs
@@ -1,5 +1,6 @@
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Repositories;
+using Application.Services.Community;
 using Domain.Entities;
 using MediatR;
 
@@ -8,17 +9,21 @@ namespace Application.Features.Post.Commands.LikePost;
 public sealed class LikePostCommandHandler : IRequestHandler<LikePostCommand>
 {
     private readonly IPostRepository _postRepository;
+    private readonly ICommunityService _communityService;
 
-    public LikePostCommandHandler(IPostRepository postRepository)
+    public LikePostCommandHandler(IPostRepository postRepository, ICommunityService communityService)
     {
         _postRepository = postRepository;
+        _communityService = communityService;
     }
 
     public async Task Handle(LikePostCommand request, CancellationToken cancellationToken)
     {
-        if (await _postRepository.GetByIdAsync(request.PostId) == null)
-            throw new NotFoundException(nameof(Domain.Entities.Post), request.PostId);
+        var post = await _postRepository.GetByIdAsync(request.PostId);
+        if (post == null) throw new NotFoundException(nameof(Domain.Entities.Post), request.PostId);
 
+        await _communityService.CheckAccessToPost(request.UserId, post);
+        
         if (await _postRepository.GetLikeAsync(request.UserId, request.PostId) != null)
             throw new BadRequestException(
                 $"The user ({request.UserId}) already has a like on the post ({request.PostId})");
