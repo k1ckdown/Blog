f05847918d1d58b56a7a6710f9237fd7ba82fa52
Ivan Semenov <DevSemTsk@gmail.com>
2023-12-12 22:39:59 +0700

    Refactor

--- Blog.Persistence/Repositories/PostRepository.cs
+++ Blog.Persistence/Repositories/PostRepository.cs
@@ -9,18 +9,18 @@ public sealed class PostRepository : Repository<Post>, IPostRepository
 {
     public PostRepository(ApplicationDbContext dbContext) : base(dbContext) {}
     
-    public async Task<Post?> GetByIdIncludingLikes(Guid id) =>
-        await DbContext.Posts
+    public async Task<Post?> GetByIdIncludingLikes(Guid id) => 
+        await Entities
             .Include(post => post.Likes)
             .FirstOrDefaultAsync(post => post.Id == id);
     
-    public async Task<Post?> GetByIdIncludingComments(Guid id) =>
-        await DbContext.Posts
+    public async Task<Post?> GetByIdIncludingComments(Guid id) => 
+        await Entities
             .Include(post => post.Comments)
             .FirstOrDefaultAsync(post => post.Id == id);
 
-    public async Task<Post?> GetByIdIncludingAllAsync(Guid id) =>
-        await DbContext.Posts
+    public async Task<Post?> GetByIdIncludingAllAsync(Guid id) => 
+        await Entities
             .Include(post => post.User)
             .Include(post => post.Likes)
             .Include(post => post.Community)
